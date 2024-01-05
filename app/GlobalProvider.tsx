17160709553ba610f0c9ba800694fa2d4063786f
Daddyjohn63 <john@webandprosper.co.uk>
2024-01-05 11:23:38 +0000

    install and configure redux

--- app/GlobalProvider.tsx
+++ app/GlobalProvider.tsx
@@ -1,14 +1,18 @@
 'use client';
 
+import { store } from '@/redux/store';
 import { SessionProvider } from 'next-auth/react';
 import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
 
 export function GlobalProvider({ children }: { children: React.ReactNode }) {
   return (
     <>
       {/* Toaster must be added at the top */}
       <Toaster />
-      <SessionProvider>{children}</SessionProvider>
+      <Provider store={store}>
+        <SessionProvider>{children}</SessionProvider>
+      </Provider>
     </>
   );
 }
